4895f8550868f7281bfbfff4a9a03a89703d2c91
Kerstin <kerstinweibeler@gmail.com>
2023-03-06 20:43:41 +0000

    feat: Create mixin for page titles

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,25 @@ from django.shortcuts import render
 from django.views import generic
 from .models import Post
 
-class PostFeaturedList(generic.ListView):
+
+class PageTitleViewMixin:
+    """
+    Create page titel that injects page titel varibale
+    into the template
+    Code found here:
+    https://www.forgepackages.com/guides/page-titles/
+    """
+    title = ""
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = self.title
+        return context
+
+
+class PostFeaturedList(PageTitleViewMixin, generic.ListView):
     model = Post
+    title = "Home"
     queryset = Post.objects.filter(
         featured=True, status=1).order_by('-created_on')
     template_name = "index.html"
