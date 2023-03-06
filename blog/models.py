2e4cc177c7f3de05c6df7dcbf80bb0941733b9f2
Kerstin <kerstinweibeler@gmail.com>
2023-03-06 19:26:12 +0000

    Create data model for blog

--- blog/models.py
+++ blog/models.py
@@ -1,14 +1,76 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+from django.urls import reverse
+
+STATUS = ((0, "Draft"), (1, "Published"))
 
 
-class Profile(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = CloudinaryField('image', default='placeholder')
-    description = models.TextField(max_length=200, blank=True, null=True)
-    created_on = models.DateTimeField(auto_now_add=True)
+class Category(models.Model):
+    """
+    Model for categories
+    """
+    title = models.CharField(max_length=30)
+    slug = models.SlugField(max_length=100, unique=True, null=False)
+    category_image = CloudinaryField('image', default='placeholder')
+
+    class Meta:
+        ordering = ('title',)
+        verbose_name_plural = 'Categories'
 
     def __str__(self):
-        return self.user.username
+        return self.title
+
+
+class Post(models.Model):
+    """
+    Model for posts
+    """
+    id = models.BigAutoField(primary_key=True)
+    title = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True, null=False)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts")
+    created_on = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+    country = models.TextField(max_length=100)
+    featured_image = CloudinaryField('image', default='placeholder')
+    regions = models.ManyToManyField(Category)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(
+        User, related_name='blogpost_like', blank=True)
+    featured = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
+
+    def number_of_likes(self):
+        return self.likes.count()
+
+    def get_absolute_url(self):
+        return reverse('post_detail', kwargs={'slug': self.slug})
+
+
+class Comment(models.Model):
+    """
+    Model for post comments
+    """
+    id = models.BigAutoField(primary_key=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name="comments")
+    name = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments')
+    body = models.TextField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
--- users/models.py
+++ users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Profile(models.Model):
+    """
+    Model for profile
+    """
+    id = models.BigAutoField(primary_key=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = CloudinaryField('image', default='placeholder')
+    description = models.TextField(max_length=200, blank=True, null=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username
