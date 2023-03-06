408384f2436287a229f10e60df7ff5a0169e14af
Kerstin <kerstinweibeler@gmail.com>
2023-03-06 18:37:26 +0000

    fix: Error Could not build wheels for backports.zoneinfo during deploy. Creating a runtime.txt file

--- travelafrica/settings.py
+++ travelafrica/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['travel-africa.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['travel-africa.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
