a472ed83539a77ae95a1462de19b014e62bd3f20
Kerstin <kerstinweibeler@gmail.com>
2023-03-07 14:20:29 +0000

    refactor: Change import in admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
-from .models import Post, Category, Profile
+from blog.models import Post, Category
+from users.models import Profile
 
 
 @admin.register(Post)
