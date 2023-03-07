9e744ec798c97d9134202024b895063018f23df7
Kerstin <kerstinweibeler@gmail.com>
2023-03-07 18:42:11 +0000

    feat: Import messages and set up message tags in settings.py

--- travelafrica/settings.py
+++ travelafrica/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import dj_database_url
+from django.contrib.messages import constants as messages
 import os
 if os.path.isfile('env.py'):
     import env
@@ -66,6 +67,14 @@ ACCOUNT_EMAIL_VERIFICATION = 'none'
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 
 CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+MESSAGE_TAGS = {
+        messages.DEBUG: 'alert-info',
+        messages.INFO: 'alert-info',
+        messages.SUCCESS: 'alert-success',
+        messages.WARNING: 'alert-warning',
+        messages.ERROR: 'alert-danger',
+    }
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
