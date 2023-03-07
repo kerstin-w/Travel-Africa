3d802e1ed76c71c884f54bf5fe30e923692489cf
Kerstin <kerstinweibeler@gmail.com>
2023-03-07 15:58:48 +0000

    build: Install Crispy Forms

--- travelafrica/settings.py
+++ travelafrica/settings.py
@@ -49,6 +49,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     'cloudinary',
     'django_summernote',
+    'crispy_forms',
+    "crispy_bootstrap5",
     'blog',
     'users',
 ]
@@ -60,6 +62,10 @@ LOOUT_REDIRECT_URL = '/'
 
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_EMAIL_VERIFICATION = 'none'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
