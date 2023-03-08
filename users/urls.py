1717d5cbfeac909dfe500b6d8c5bbcd180d18674
Kerstin <kerstinweibeler@gmail.com>
2023-03-08 12:07:45 +0000

    refactor: Adjust urlpath for profile_update

--- users/urls.py
+++ users/urls.py
@@ -6,5 +6,5 @@ app_name = 'users'
 urlpatterns = [
     path("<str:username>/", views.ProfileHomeView.as_view(), name='profile_home'),
     path("<str:username>/<int:pk>/delete/", views.ProfileDeleteView.as_view(), name='profile_delete'),
-    path('profile/<int:pk>/update', views.ProfileUpdateView.as_view(), name='profile_update'),
+    path('<int:pk>/update', views.ProfileUpdateView.as_view(), name='profile_update'),
 ]
