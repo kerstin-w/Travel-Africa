629891fdd8a8afa4ff78d04b4dfd43bfd749b3d9
Kerstin <kerstinweibeler@gmail.com>
2023-03-09 14:20:35 +0000

    feat: Create template for post_list and add pagination to view

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.contrib import messages
+from django.core.paginator import Paginator
 from django.views import generic
+from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.views.generic.edit import CreateView
 from django.views.generic.list import ListView
@@ -48,6 +50,7 @@ class PostListView(ListView):
     model = Post
     title = "Posts"
     template_name = "post_list.html"
+    paginate_by = 2
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
