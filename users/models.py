df9b8151dc874c27d1bc85cde99a07be3173d858
Kerstin <kerstinweibeler@gmail.com>
2023-03-10 16:28:51 +0000

    feat: Add signals to Profile model

--- users/models.py
+++ users/models.py
@@ -1,20 +1,26 @@
-from django.db import models
-from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
-from django.contrib.auth.models import AbstractUser, UserManager
-
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
+from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Profile(models.Model):
     """
     Model for profile
     """
+
     id = models.BigAutoField(primary_key=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = CloudinaryField('image', default='placeholder')
+    image = CloudinaryField("image", default="placeholder")
     description = models.TextField(max_length=200, blank=True, null=True)
     created_on = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.user.username
-        
+
+    @receiver(post_save, sender=get_user_model())
+    def create_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
