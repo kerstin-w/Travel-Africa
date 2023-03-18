c926815fca39d31ba22ba477ccb4f55bf2027ed5
Kerstin <kerstinweibeler@gmail.com>
2023-03-18 16:08:30 +0000

    refactor: break path lines

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,22 @@
 from . import views
 from django.urls import path
 
-app_name = 'users'
+app_name = "users"
 
 urlpatterns = [
-    path("profile/<str:username>/", views.ProfileHomeView.as_view(), name='profile_home'),
-    path("<str:username>/<int:pk>/delete/", views.ProfileDeleteView.as_view(), name='profile_delete'),
-    path('<int:pk>/update', views.ProfileUpdateView.as_view(), name='profile_update'),
+    path(
+        "profile/<str:username>/",
+        views.ProfileHomeView.as_view(),
+        name="profile_home",
+    ),
+    path(
+        "<str:username>/<int:pk>/delete/",
+        views.ProfileDeleteView.as_view(),
+        name="profile_delete",
+    ),
+    path(
+        "<int:pk>/update",
+        views.ProfileUpdateView.as_view(),
+        name="profile_update",
+    ),
 ]
