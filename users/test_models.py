027626608290cf2d000e436336824bb413cfb52e
Kerstin <kerstinweibeler@gmail.com>
2023-03-21 11:37:53 +0000

    test: Add test for Profile model for create profile and update profile

--- users/test_models.py
+++ users/test_models.py
@@ -9,15 +9,17 @@ from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
 from .models import Profile
 
+
 class TestProfileModel(TestCase):
     """
     A class to test the Profile model
     """
+
     @classmethod
     def setUpTestData(cls):
         User = get_user_model()
         cls.test_user = User.objects.create_user(
-            'testuser', 'testuser@test.com', 'testpass'
+            "testuser", "testuser@test.com", "testpass"
         )
 
     def test_profile_str(self):
@@ -26,3 +28,29 @@ class TestProfileModel(TestCase):
         """
         profile = get_object_or_404(Profile, user=self.test_user)
         self.assertEqual(str(profile), "testuser")
+
+    def test_create_profile_receiver(self):
+        """
+        Test Profile create receiver
+        """
+        User = get_user_model()
+        # Create new standard user
+        new_user = User.objects.create_user(
+            "newuser", "newuser@test.com", "newpass"
+        )
+        profile = get_object_or_404(Profile, user=new_user)
+        # Test new user username
+        self.assertTrue(profile)
+        self.assertEqual(profile.user.username, "newuser")
+
+    def test_update_profile_receiver(self):
+        """Test Profile update receiver"""
+        # Get test user
+        profile = get_object_or_404(Profile, user=self.test_user)
+        # Update description
+        profile.description = "Test description"
+        profile.save()
+        updated_profile = get_object_or_404(Profile, user=self.test_user)
+        # Test updated description
+        self.assertTrue(updated_profile)
+        self.assertEqual(updated_profile.description, "Test description")
