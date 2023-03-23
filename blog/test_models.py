6afbe14d3dd99d3e0162f773d9ce00d0c4b44cc8
Kerstin <kerstinweibeler@gmail.com>
2023-03-23 13:37:09 +0000

    test: Add test for ordering for Category Model

--- blog/test_models.py
+++ blog/test_models.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 from .models import Category
 
+
 class CategoryModelTest(TestCase):
     """
     Test Cases for Category Model
@@ -22,7 +23,7 @@ class CategoryModelTest(TestCase):
         max_length = category._meta.get_field("title").max_length
         # Check maximum length of field
         self.assertEqual(max_length, 30)
-    
+
     def test_slug_max_length(self):
         """
         Test maximal length of Slug
@@ -31,7 +32,7 @@ class CategoryModelTest(TestCase):
         max_length = category._meta.get_field("slug").max_length
         # Check maximum length of field
         self.assertEqual(max_length, 30)
-    
+
     def test_title_unique(self):
         """
         Test unique title
@@ -39,7 +40,7 @@ class CategoryModelTest(TestCase):
         category = Category(title="Test Category", slug="test-category-2")
         with self.assertRaises(Exception):
             category.save()
-    
+
     def test_slug_unique(self):
         """
         Test unique slug
@@ -47,3 +48,24 @@ class CategoryModelTest(TestCase):
         category = Category(title="Test Category 2", slug="test-category")
         with self.assertRaises(Exception):
             category.save()
+
+    def test_ordering(self):
+        """
+        Test ordering of categories
+        """
+        Category.objects.create(title="B Category", slug="b-category")
+        Category.objects.create(title="A Category", slug="a-category")
+        Category.objects.create(title="C Category", slug="c-category")
+
+        expected_categories = [
+            "A Category",
+            "B Category",
+            "C Category",
+            "Test Category",
+        ]
+        # Categories in actual order
+        actual_categories = [
+            category.title for category in Category.objects.all()
+        ]
+        # Compare actual order with expected order
+        self.assertListEqual(actual_categories, expected_categories)
