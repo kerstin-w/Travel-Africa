8e0f2050967069ee00349ec0dde370729e3c7fff
Kerstin <kerstinweibeler@gmail.com>
2023-03-24 06:55:51 +0000

    test: Create test class for SuperuserFormFieldsMixin and test that superuser can access all fields

--- blog/tests/test_views.py
+++ blog/tests/test_views.py
@@ -1,5 +1,9 @@
 from django.test import TestCase, RequestFactory
 from django.views.generic import TemplateView
+from django.contrib.auth.models import User
+from django.urls import reverse
+from django.views import View
+from blog.views import SuperuserFormFieldsMixin, PostCreateView
 
 from blog.views import PageTitleViewMixin
 
@@ -36,3 +40,33 @@ class PageTitleViewMixinTest(TestCase):
         view = TestView.as_view()
         response = view(request)
         self.assertEqual(response.context_data["title"], "Test Page")
+
+
+class SuperuserFormFieldsMixinTest(TestCase):
+    """
+    Test cases for SuperuserFormFieldsMixin
+    """
+
+    def setUp(self):
+        """
+        Test Data
+        """
+        self.factory = RequestFactory()
+        self.user = User.objects.create_user(
+            username="testuser", password="testpass"
+        )
+        self.superuser = User.objects.create_superuser(
+            username="superuser", password="superpass"
+        )
+
+    def test_super_user_can_access_all_form_fields(self):
+        """
+        Test that the superuser can access all form fields
+        """
+        self.client.login(username="superuser", password="superpass")
+        response = self.client.get(reverse("post_create"))
+        self.assertEqual(response.status_code, 200)
+
+        # Check the featured field is in the form
+        form = response.context_data["form"]
+        self.assertIn("featured", form.fields)
