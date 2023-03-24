0a0e4d812a15bb6d6636856f5318aa1fb340fb20
Kerstin <kerstinweibeler@gmail.com>
2023-03-24 06:41:40 +0000

    improvement: Change database settings to if database in environ use postgres

--- travelafrica/settings.py
+++ travelafrica/settings.py
@@ -120,18 +120,20 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "travelafrica.wsgi.application"
 
-if 'test' in sys.argv:
-    # Configuration for test database
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
         }
-    }
-else:
-    # Default configuration
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
     }
 
 
